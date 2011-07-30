e99252a61c4e1a7409404096e0a933038eaa5b97
prahlad1981@gmail.com <prahlad1981@gmail.com@e64ef92d-3a02-a94f-dadf-5fd11c07f25a>
2011-07-30 09:19:59 +0000

    Created the repository.

---
+++ LoginBox.Designer.vb
@@ -0,0 +1,112 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class LoginBox
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnLogin = New System.Windows.Forms.Button
+        Me.btnCancel = New System.Windows.Forms.Button
+        Me.Label1 = New System.Windows.Forms.Label
+        Me.txtUsername = New System.Windows.Forms.TextBox
+        Me.txtPassword = New System.Windows.Forms.TextBox
+        Me.Label2 = New System.Windows.Forms.Label
+        Me.SuspendLayout()
+        '
+        'btnLogin
+        '
+        Me.btnLogin.Location = New System.Drawing.Point(57, 96)
+        Me.btnLogin.Name = "btnLogin"
+        Me.btnLogin.Size = New System.Drawing.Size(75, 23)
+        Me.btnLogin.TabIndex = 2
+        Me.btnLogin.Text = "Login"
+        Me.btnLogin.UseVisualStyleBackColor = True
+        '
+        'btnCancel
+        '
+        Me.btnCancel.Location = New System.Drawing.Point(138, 96)
+        Me.btnCancel.Name = "btnCancel"
+        Me.btnCancel.Size = New System.Drawing.Size(75, 23)
+        Me.btnCancel.TabIndex = 3
+        Me.btnCancel.Text = "Cancel"
+        Me.btnCancel.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 18)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(74, 15)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "User Name:"
+        '
+        'txtUsername
+        '
+        Me.txtUsername.Location = New System.Drawing.Point(87, 15)
+        Me.txtUsername.Name = "txtUsername"
+        Me.txtUsername.Size = New System.Drawing.Size(171, 21)
+        Me.txtUsername.TabIndex = 0
+        Me.txtUsername.Text = "Anonymous"
+        '
+        'txtPassword
+        '
+        Me.txtPassword.Location = New System.Drawing.Point(87, 42)
+        Me.txtPassword.Name = "txtPassword"
+        Me.txtPassword.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42)
+        Me.txtPassword.Size = New System.Drawing.Size(171, 21)
+        Me.txtPassword.TabIndex = 1
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(20, 45)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(66, 15)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "Password:"
+        '
+        'LoginBox
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(270, 131)
+        Me.Controls.Add(Me.txtPassword)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.txtUsername)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnCancel)
+        Me.Controls.Add(Me.btnLogin)
+        Me.Font = New System.Drawing.Font("Arial", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.MaximizeBox = False
+        Me.Name = "LoginBox"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "LoginBox"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents btnLogin As System.Windows.Forms.Button
+    Friend WithEvents btnCancel As System.Windows.Forms.Button
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents txtUsername As System.Windows.Forms.TextBox
+    Friend WithEvents txtPassword As System.Windows.Forms.TextBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+End Class
---
+++ MainBox.vb
@@ -0,0 +1,150 @@
+﻿Imports System.Net
+Imports System.IO
+Imports System.Text
+Imports System.Text.RegularExpressions
+
+Public Enum Images
+    DRIVE = 0
+    OPEN_FOLDER = 1
+    CLOSED_FOLDER = 2
+    FILE = 3
+End Enum
+
+Public Class MainBox
+    Private Request As FtpWebRequest
+    Private RemoteDir As FtpDirectory
+    Private LastDirectory As String = ""
+    Private WithEvents client As FtpClient
+
+    Private Sub MainBox_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        'Fill the local file system
+        Dim drives As DriveInfo() = DriveInfo.GetDrives()
+        For i As Integer = 0 To drives.Length - 1
+            Dim drive As String = drives(i).Name.Replace("\", "")
+            tvwLocal.Nodes.Add(drive, drive, Images.DRIVE, Images.DRIVE)
+            Dim node As TreeNode = tvwLocal.Nodes(drive)
+            Dim eventargs As New TreeViewEventArgs(node)
+            tvwLocal_AfterExpand(Nothing, eventargs)
+        Next
+    End Sub
+
+    Private Function ExtractFileName(ByVal path As String) As String
+        Dim files As String() = path.Split("\")
+        Dim file As String = files(files.Length - 1)
+        Return file
+    End Function
+
+    Private Sub tvwLocal_AfterExpand(ByVal sender As Object, ByVal e As System.Windows.Forms.TreeViewEventArgs) Handles tvwLocal.AfterExpand
+        On Error GoTo em
+        Dim current As TreeNode = e.Node
+        'Dim nextFile As String = Dir(current.Name & "\", FileAttribute.Directory)
+        Dim dirInfo As String() = Directory.GetDirectories(current.Name & "\")
+        current.Nodes.Clear()
+        'Do While (nextFile IsNot Nothing AndAlso nextFile.Length > 0)
+        For Each nextFile As String In dirInfo
+            Dim dirnode As New TreeNode
+            dirnode.Text = ExtractFileName(nextFile)
+            dirnode.Name = nextFile ' current.Name & "\" & nextFile
+            dirnode.ImageIndex = Images.CLOSED_FOLDER
+            dirnode.SelectedImageIndex = Images.CLOSED_FOLDER
+            dirnode.Nodes.Add("*DUMMY*")
+            'current.Nodes.Add(current.Name & "\" & nextFile, nextFile, Images.CLOSED_FOLDER, Images.CLOSED_FOLDER)
+            current.Nodes.Add(dirnode)
+            'Loop
+        Next
+        dirInfo = Directory.GetFiles(current.Name & "\")
+        'nextFile = Dir(current.Name & "\")
+        For Each nextFile As String In dirInfo
+            current.Nodes.Add(nextFile, ExtractFileName(nextFile), Images.FILE, Images.FILE)
+        Next
+        Exit Sub
+em:
+        If Err.Number <> 52 And Err.Number <> 57 Then
+            MessageBox.Show(Err.Description)
+        End If
+    End Sub
+
+    Private Sub btnOk_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnOk.Click
+        On Error GoTo em
+        If txtUrl.Text.Length = 0 Then
+            MessageBox.Show("Please enter valid Url")
+            Exit Sub
+        End If
+        Dim url As New Uri(txtUrl.Text)
+        If url.Scheme <> Uri.UriSchemeFtp Then
+            MessageBox.Show("Not an FTP Url")
+            Exit Sub
+        End If
+        Dim login As New LoginBox()
+        Dim cred As String() = login.GetCredentials()
+        txtLog.AppendText("Preparing to connect...")
+        If cred IsNot Nothing Then
+            client = New FtpClient(txtUrl.Text, cred(0), cred(1))
+            ListRemoteDirectory("")
+        End If
+        Exit Sub
+em:
+        MessageBox.Show(Err.Description)
+    End Sub
+
+    Private Sub ListRemoteDirectory(ByVal directory As String)
+        lvwRemote.Items.Clear()
+        'Request = CType(FtpWebRequest.Create(txtUrl.Text), FtpWebRequest)
+        'Request.Credentials = New NetworkCredential(cred(0), cred(1))
+        RemoteDir = client.ListDirectory(directory)
+        LastDirectory = directory
+        lvwRemote.Items.Add("..", "..")
+        For Each item As FtpFileInfo In RemoteDir
+            If item.FileType = FtpFileInfo.DirectoryEntryTypes.Directory Then
+                lvwRemote.Items.Add(item.FileName, Images.CLOSED_FOLDER)
+            Else
+                lvwRemote.Items.Add(item.FileName, Images.FILE)
+            End If
+        Next
+
+    End Sub
+
+    Private Sub client_StatusChanged(ByVal newStatus As String) Handles client.StatusChanged
+        txtLog.AppendText(newStatus & Environment.NewLine)
+        Application.DoEvents()
+    End Sub
+
+    Private Sub lvwRemote_DoubleClick(ByVal sender As Object, ByVal e As System.EventArgs) Handles lvwRemote.DoubleClick
+        If lvwRemote.SelectedItems.Count > 0 Then
+            Dim item As ListViewItem = lvwRemote.SelectedItems(0)
+            If item.ImageIndex <> Images.FILE Then
+                ListRemoteDirectory(item.Text)
+            End If
+        End If
+    End Sub
+
+    Private Sub lvwRemote_ItemMouseHover(ByVal sender As System.Object, ByVal e As System.Windows.Forms.ListViewItemMouseHoverEventArgs) Handles lvwRemote.ItemMouseHover
+        lblStatus.Text = client.CurrentDirectory & IIf(client.CurrentDirectory.Length = 1, "", "/") & e.Item.Text
+    End Sub
+
+    Private Sub btnUpload_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnUpload.Click
+        If tvwLocal.SelectedNode IsNot Nothing AndAlso tvwLocal.SelectedNode.ImageIndex = Images.FILE Then
+            Dim fi As New FileInfo(tvwLocal.SelectedNode.FullPath)
+            client.Upload(fi)
+            ListRemoteDirectory(".")
+        End If
+    End Sub
+
+    Private Sub lvwRemote_SelectedIndexChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles lvwRemote.SelectedIndexChanged
+
+    End Sub
+
+    Private Sub btnDownload_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnDownload.Click
+        If tvwLocal.SelectedNode IsNot Nothing AndAlso tvwLocal.SelectedNode.ImageIndex = Images.CLOSED_FOLDER AndAlso _
+        lvwRemote.SelectedItems(0).ImageIndex = Images.FILE Then
+            Dim sourceFile As String = lvwRemote.SelectedItems(0).Text
+            Dim fi As New FileInfo(tvwLocal.SelectedNode.FullPath & "\" & sourceFile)
+            client.Download(sourceFile, fi, True)
+            Dim node As TreeNode = tvwLocal.SelectedNode
+            Dim ev As New TreeViewEventArgs(node)
+            tvwLocal_AfterExpand(Nothing, ev)
+            node.Expand()
+            node.Nodes(fi.FullName).EnsureVisible()
+        End If
+    End Sub
+End Class
